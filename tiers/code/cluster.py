2292302ed9eb25cf12e0484c5bf8fd099c56b320
Quinn McNamara <quinn.mcnamara@hudl.com>
2015-08-05 23:04:42 -0500

    first go at determining optimal k

--- tiers/code/cluster.py
+++ tiers/code/cluster.py
@@ -1,6 +1,9 @@
 from sklearn.cluster import KMeans
+from sklearn import metrics
+from sklearn.metrics import pairwise_distances
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data from CSV
@@ -19,7 +22,30 @@ feature_columns = [
 
 fitting_data = df[feature_columns]
 
-clstr = KMeans(n_clusters=9)
+# Elbow Method for determining k
+# inertias = []
+# for k in range(2, 100):
+# 	clstr = KMeans(n_clusters=k)
+# 	clstr.fit(fitting_data)
+# 	inertias.append(clstr.inertia_)
+# 	print clstr.inertia_
+# plt.plot(inertias)
+# plt.show()
+
+# Silhouette Method for determining k 
+silhouettes = []
+for k in range(8, 100):
+	clstr = KMeans(n_clusters=k)
+	clstr.fit(fitting_data)
+	labels = clstr.labels_
+	sscore = metrics.silhouette_score(fitting_data, labels, metric='euclidean')
+	print sscore
+	silhouettes.append(sscore)
+plt.plot(silhouettes)
+plt.show()
+
+'''
+clstr = KMeans(n_clusters=91) # Using rule of thumb (k=sqrt(n/2))
 clstr.fit(fitting_data)
 
 df['tier'] = clstr.labels_
@@ -28,3 +54,4 @@ results = df[['Player', 'tier']]
 print results['tier'].value_counts()
 
 results.to_csv('results.csv')
+'''
