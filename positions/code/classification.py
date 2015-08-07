df9ade48df851002abe085687b2b85be9749d2b2
qmac <quinn.mcnamara@hudl.com>
2015-08-06 20:28:07 -0500

    added visualization for positions

--- positions/code/classification.py
+++ positions/code/classification.py
@@ -1,7 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
+from positionchart import radar_graph
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data from CSV
@@ -11,11 +11,28 @@ df = pd.DataFrame.from_csv('./../data/stats.dat', sep=' ')
 df = df[(df['pos'] != '??') & (df['pts'] > 100)]
 
 # Run the classifier
-clf = KNeighborsClassifier()
+clf = KNeighborsClassifier(n_neighbors=10)
 training = df.drop('ginobili,manu')
 
 clf.fit(training.drop('pos', 1), training['pos'])
-print clf.score(training.drop('pos', 1), training['pos'])
 
 player = df.loc['ginobili,manu']
 print clf.predict(player.drop('pos', 1))
+
+prediction_probabilities = clf.predict_proba(player.drop('pos', 1))
+print prediction_probabilities
+
+# These have to be hard coded since year info is not in the data set
+years = ['2004-2005', '2005-2006', '2006-2007', '2007-2008', '2008-2009', '2009-2010', '2010-2011', '2011-2012', '2012-2013']
+
+
+for i in range(len(prediction_probabilities)):
+	labels = ['C', 'PF', 'PG', 'SF', 'SG']
+	values = prediction_probabilities[i]
+	optimum = [1.0, 1.0, 1.0, 1.0, 1.0]
+
+	radar_graph(labels, values, optimum)
+
+	plt.suptitle('Manu Ginobili %s' % (years[i]))
+	plt.savefig('./../figures/manu_ginobili_%s' % (years[i]))
+	plt.show()
---
+++ positions/code/positionchart.py
@@ -0,0 +1,70 @@
+'''
+This code was copied from https://gist.github.com/sergiobuj/6721187
+Credit to sergiobuj who adapted the radar chart code from http://matplotlib.org/examples/api/radar_chart.html
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+from matplotlib.spines import Spine
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+
+def _radar_factory(num_vars):
+    theta = 2*np.pi * np.linspace(0, 1-1./num_vars, num_vars)
+    theta += np.pi/2
+
+    def unit_poly_verts(theta):
+        x0, y0, r = [0.5] * 3
+        verts = [(r*np.cos(t) + x0, r*np.sin(t) + y0) for t in theta]
+        return verts
+
+    class RadarAxes(PolarAxes):
+        name = 'radar'
+        RESOLUTION = 1
+
+        def fill(self, *args, **kwargs):
+            closed = kwargs.pop('closed', True)
+            return super(RadarAxes, self).fill(closed=closed, *args, **kwargs)
+
+        def plot(self, *args, **kwargs):
+            lines = super(RadarAxes, self).plot(*args, **kwargs)
+            for line in lines:
+                self._close_line(line)
+
+        def _close_line(self, line):
+            x, y = line.get_data()
+            # FIXME: markers at x[0], y[0] get doubled-up
+            if x[0] != x[-1]:
+                x = np.concatenate((x, [x[0]]))
+                y = np.concatenate((y, [y[0]]))
+                line.set_data(x, y)
+
+        def set_varlabels(self, labels):
+            self.set_thetagrids(theta * 180/np.pi, labels)
+
+        def _gen_axes_patch(self):
+            verts = unit_poly_verts(theta)
+            return plt.Polygon(verts, closed=True, edgecolor='k')
+
+        def _gen_axes_spines(self):
+            spine_type = 'circle'
+            verts = unit_poly_verts(theta)
+            verts.append(verts[0])
+            path = Path(verts)
+            spine = Spine(self, spine_type, path)
+            spine.set_transform(self.transAxes)
+            return {'polar': spine}
+
+    register_projection(RadarAxes)
+    return theta
+
+def radar_graph(labels = [], values = [], optimum = []):
+    N = len(labels) 
+    theta = _radar_factory(N)
+    max_val = max(max(optimum), max(values))
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection='radar')
+    ax.plot(theta, values, color='k')
+    ax.plot(theta, optimum, color='r')
+    ax.set_varlabels(labels)
