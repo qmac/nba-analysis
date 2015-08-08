392ae720f5bf7f600fd746d57d5b732e7a023224
qmac <quinn.mcnamara@hudl.com>
2015-08-08 15:33:21 -0500

    added fixed cluster starts and saving figures

--- tiers/code/cluster.py
+++ tiers/code/cluster.py
@@ -1,6 +1,5 @@
 from sklearn.cluster import KMeans
-from sklearn import metrics
-from sklearn.metrics import pairwise_distances
+from sklearn.metrics import silhouette_score
 
 import pandas as pd
 import numpy as np
@@ -23,29 +22,28 @@ feature_columns = [
 fitting_data = df[feature_columns]
 
 # Elbow Method for determining k
-# inertias = []
-# for k in range(2, 100):
-# 	clstr = KMeans(n_clusters=k)
-# 	clstr.fit(fitting_data)
-# 	inertias.append(clstr.inertia_)
-# 	print clstr.inertia_
-# plt.plot(inertias)
-# plt.show()
+inertias = []
+for k in range(2, 100):
+	clstr = KMeans(n_clusters=k, random_state=42)
+	clstr.fit(fitting_data)
+	inertias.append(clstr.inertia_)
+
+plt.plot(inertias)
+plt.savefig('./../figures/inertias.png')
 
 # Silhouette Method for determining k 
 silhouettes = []
-for k in range(8, 100):
-	clstr = KMeans(n_clusters=k)
-	clstr.fit(fitting_data)
-	labels = clstr.labels_
-	sscore = metrics.silhouette_score(fitting_data, labels, metric='euclidean')
-	print sscore
-	silhouettes.append(sscore)
+for k in range(2, 100):
+	clstr = KMeans(n_clusters=k, random_state=42)
+	cluster_labels = clstr.fit_predict(fitting_data)
+	silhouette = silhouette_score(fitting_data, cluster_labels)
+	silhouettes.append(silhouette)
+
 plt.plot(silhouettes)
-plt.show()
+plt.savefig('./../figures/silhouettes.png')
 
 '''
-clstr = KMeans(n_clusters=91) # Using rule of thumb (k=sqrt(n/2))
+clstr = KMeans(n_clusters=11) # Using rule of thumb (k=sqrt(n/2))
 clstr.fit(fitting_data)
 
 df['tier'] = clstr.labels_
