3fd0161150c3cf45cd84eb5afd8f724f894819f0
qmac <quinten.mcnamara@gmail.com>
2015-08-30 14:05:24 -0500

    first commit of ball tracking

---
+++ tracking/code/ball_tracking.py
@@ -0,0 +1,119 @@
+import requests
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from matplotlib.patches import Circle, Rectangle, Arc
+
+# Function to draw the basketball court lines
+def draw_court(ax=None, color="gray", lw=1, zorder=0):
+    
+    if ax is None:
+        ax = plt.gca()
+
+    # Creates the out of bounds lines around the court
+    outer = Rectangle((0,-50), width=94, height=50, color=color,
+                      zorder=zorder, fill=False, lw=lw)
+
+    # The left and right basketball hoops
+    l_hoop = Circle((5.35,-25), radius=.75, lw=lw, fill=False, 
+                    color=color, zorder=zorder)
+    r_hoop = Circle((88.65,-25), radius=.75, lw=lw, fill=False,
+                    color=color, zorder=zorder)
+    
+    # Left and right backboards
+    l_backboard = Rectangle((4,-28), 0, 6, lw=lw, color=color,
+                            zorder=zorder)
+    r_backboard = Rectangle((90, -28), 0, 6, lw=lw,color=color,
+                            zorder=zorder)
+
+    # Left and right paint areas
+    l_outer_box = Rectangle((0, -33), 19, 16, lw=lw, fill=False,
+                            color=color, zorder=zorder)    
+    l_inner_box = Rectangle((0, -31), 19, 12, lw=lw, fill=False,
+                            color=color, zorder=zorder)
+    r_outer_box = Rectangle((75, -33), 19, 16, lw=lw, fill=False,
+                            color=color, zorder=zorder)
+
+    r_inner_box = Rectangle((75, -31), 19, 12, lw=lw, fill=False,
+                            color=color, zorder=zorder)
+
+    # Left and right free throw circles
+    l_free_throw = Circle((19,-25), radius=6, lw=lw, fill=False,
+                          color=color, zorder=zorder)
+    r_free_throw = Circle((75, -25), radius=6, lw=lw, fill=False,
+                          color=color, zorder=zorder)
+
+    # Left and right corner 3-PT lines
+    # a represents the top lines
+    # b represents the bottom lines
+    l_corner_a = Rectangle((0,-3), 14, 0, lw=lw, color=color,
+                           zorder=zorder)
+    l_corner_b = Rectangle((0,-47), 14, 0, lw=lw, color=color,
+                           zorder=zorder)
+    r_corner_a = Rectangle((80, -3), 14, 0, lw=lw, color=color,
+                           zorder=zorder)
+    r_corner_b = Rectangle((80, -47), 14, 0, lw=lw, color=color,
+                           zorder=zorder)
+    
+    # Left and right 3-PT line arcs
+    l_arc = Arc((5,-25), 47.5, 47.5, theta1=292, theta2=68, lw=lw,
+                color=color, zorder=zorder)
+    r_arc = Arc((89, -25), 47.5, 47.5, theta1=112, theta2=248, lw=lw,
+                color=color, zorder=zorder)
+
+    # half_court
+    # ax.axvline(470)
+    half_court = Rectangle((47,-50), 0, 50, lw=lw, color=color,
+                           zorder=zorder)
+
+    hc_big_circle = Circle((47, -25), radius=6, lw=lw, fill=False,
+                           color=color, zorder=zorder)
+    hc_sm_circle = Circle((47, -25), radius=2, lw=lw, fill=False,
+                          color=color, zorder=zorder)
+
+    court_elements = [l_hoop, l_backboard, l_outer_box, outer,
+                      l_inner_box, l_free_throw, l_corner_a,
+                      l_corner_b, l_arc, r_hoop, r_backboard, 
+                      r_outer_box, r_inner_box, r_free_throw,
+                      r_corner_a, r_corner_b, r_arc, half_court,
+                      hc_big_circle, hc_sm_circle]
+
+    # Add the court elements onto the axes
+    for element in court_elements:
+        ax.add_patch(element)
+
+    return ax
+
+ball_scatter_x = []
+ball_scatter_y = []
+
+k = 0
+j = 0
+for i in range (1, 574):
+    print i
+
+    url = "http://stats.nba.com/stats/locations_getmoments/?eventid=%d&gameid=0041400132" % (i)
+    response = requests.get(url)
+    try:
+        moments = response.json()["moments"]
+    except ValueError:
+        print "Yikes no json"
+        continue
+
+    for moment in moments:
+        first_half = moment[0] <= 2
+        objects = moment[5]
+        ball_object = objects[0]
+        if ball_object[4] < 7.0 and ((first_half and ball_object[2] > 50) or (not first_half and ball_object[2] <= 50)):
+            ball_scatter_x.append(ball_object[2])
+            ball_scatter_y.append(ball_object[3])
+
+g = sns.jointplot(np.array(ball_scatter_x), -np.array(ball_scatter_y), kind="kde", space=0, color="b", zorder=0)
+ax = g.ax_joint
+draw_court(ax, zorder=1)
+ax.set_xlim(0, 101)
+ax.set_ylim(-50, 0)
+plt.show()
