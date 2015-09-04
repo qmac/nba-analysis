d6a3aebc646fef8821051191bd5dbd1649a292b2
qmac <quinten.mcnamara@gmail.com>
2015-09-04 12:43:19 -0500

    ITS READY

--- tracking/code/scrape.py
+++ tracking/code/scrape.py
@@ -1,8 +1,10 @@
 import sys
 import time
 import requests
+import csv
 
 from threading import Thread
+from threading import Lock
 
 from Queue import Queue
 
@@ -25,20 +27,55 @@ def worker():
         url_queue.task_done()
 
 def fetch_url(url):
+    csv_data = []
     try:
-        response = requests.get(url)
-        moments = response.json()["moments"]
+        response_json = requests.get(url).json()
+        moments = response_json["moments"]
+        home_team = response_json["home"]["abbreviation"]
+        away_team = response_json["visitor"]["abbreviation"]
         for moment in moments:
             first_half = moment[0] <= 2
-            objects = moment[5]
-            ball_object = objects[0]
-            #if ball_object[4] < 7.0 and ((first_half and ball_object[2] > 50) or (not first_half and ball_object[2] <= 50)):
-            #ball_scatter_x.append(ball_object[2])
-            #ball_scatter_y.append(ball_object[3])
-        print len(moments)
-    except:
+            ball_up_court = moment[3] <= 19.5 # Check the shot clock 
+
+            ball_object = moment[5][0]
+
+            ball_x = ball_object[2]
+            ball_y = ball_object[3]
+            ball_height = ball_object[4]
+
+            # Filter out shots and before ball is up the court
+            if ball_height < 7.0 and ball_up_court:
+                # Determine possession by ball location and half
+                off_team = ""
+                def_team = ""
+
+                if ((first_half and ball_x <= 50) or (not first_half and ball_x > 50)):
+                    off_team = home_team
+                    def_team = away_team
+                else:
+                    off_team = away_team
+                    def_team = home_team
+                
+                if off_team == "":
+                    print "FUCCCKKK"
+                if def_team == "":
+                    print "FUCKC EVEN MROE"
+
+                csv_data.append([off_team, def_team, ball_x, ball_y])
+
+    except ValueError, KeyError:
         print "error"
 
+    write_to_csv(csv_data)
+
+def write_to_csv(data):
+    lock.acquire()
+    with open('ball_data.csv', 'a+') as db:
+        writer = csv.writer(db, delimiter=',')
+        writer.writerows(data)
+    lock.release()
+
+lock = Lock()
 url_queue = Queue(NUM_CONCURRENT * 2)
 for i in range(NUM_CONCURRENT):
     t = Thread(target=worker)
