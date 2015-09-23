51a5564ba83ac6a417e86e0c60ef1836f6129a15
qmac <quinten.mcnamara@gmail.com>
2015-09-22 23:37:40 -0500

    updated analyses to use scraped data

--- shots/code/classification.py
+++ shots/code/classification.py
@@ -4,18 +4,21 @@ import pandas as pd
 import numpy as np
 import shotcharts as charts
 
-df = pd.DataFrame.from_csv('./../data/steph_shots.csv')
-
-feature_columns = ['period', 
-                'minutes_remaining', 
-                'shot_distance', 
-                'x', 
-                'y', 
+feature_columns = ['location', 
+                'period', 
+                'shot_dist', 
                 'dribbles', 
                 'touch_time', 
-                'defender_distance', 
+                'close_def_dist', 
                 'shot_clock'
                 ]
+
+class_column = 'fgm'
+
+df = pd.DataFrame.from_csv('./../data/shot_data.csv')
+df = df[(df['player_name'] == 'Curry, Stephen')]
+df = df.dropna(subset=feature_columns)
+df['location'] = (df['location'] == 'H').astype(int)
 
 classifier = GaussianNB()
 training_proportion = 0.75
@@ -26,11 +29,11 @@ training_set = df[df['is_training']==True]
 testing_set = df[df['is_training']==False]
 
 trainingFeatures = training_set[feature_columns]
-trainingTargets = np.array(training_set['shot_made_flag']).astype(bool)
+trainingTargets = np.array(training_set[class_column]).astype(bool)
 
 classifier.fit(trainingFeatures, trainingTargets)
 
 prediction_set = testing_set.copy()
-prediction_set['shot_made_flag'] = classifier.predict(testing_set[feature_columns])
+prediction_set[class_column] = classifier.predict(testing_set[feature_columns])
 
-charts.show_comparison(prediction_set, testing_set)
+#charts.show_comparison(prediction_set, testing_set)
