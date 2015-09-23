39f436e65ccc3ea6d18dd39cee58feba173c1317
qmac <quinten.mcnamara@gmail.com>
2015-09-22 21:50:44 -0500

    refactoring and fixed getting wrong url

--- shots/code/scrape.py
+++ shots/code/scrape.py
@@ -1,16 +1,15 @@
 import requests
 import csv
 
-def scrape_shots(name, team, url):
-    shots_data = []
+def scrape(url):
     try:
-        response_json = requests.get(players_url).json() 
-        shots_data = response_json["resultSets"][0]["rowSet"]
-        shots_data = [shot + [name, team] for shot in shots_data]
+        response_json = requests.get(url).json() 
+        data = response_json["resultSets"][0]["rowSet"]
+        return data
     except:
         print "error"
 
-    write_to_csv(shots_data)
+    return
 
 def write_to_csv(data):
     with open('./../data/shot_data.csv', 'a+') as db:
@@ -23,9 +22,9 @@ write_to_csv([["game_id","matchup","location","w","final_margin",
     "touch_time","shot_dist","pts_type","shot_result","closest_defender",
     "closest_defender_player_id","close_def_dist","fgm","pts", "player_name", "player_team"]])
 
+# Get player information
 players_url = "http://stats.nba.com/stats/commonallplayers?LeagueID=00&Season=2014-15&IsOnlyCurrentSeason=1"
-response_json = requests.get(players_url).json() 
-players_data = response_json["resultSets"][0]["rowSet"]
+players_data = scrape(players_url)
 
 for player_record in players_data:
     player_id = player_record[0]
@@ -37,7 +36,9 @@ for player_record in players_data:
     &GameSegment=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&Period=0\
     &PlayerID=%s&Season=2014-15&SeasonSegment=&SeasonType=Regular+Season&TeamID=0&VsConference=&VsDivision=" % (player_id)
     
-    scrape_shots(name, team, shots_url)
+    shots_data = scrape(shots_url)
+    shots_data = [shot + [name, team] for shot in shots_data]
+    write_to_csv(shots_data)
 
 
 
