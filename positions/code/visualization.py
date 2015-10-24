4e23b1e0d3709f604b161b661960f8a85475e94c
qmac <quinten.mcnamara@gmail.com>
2015-10-24 17:37:12 -0500

    UI improvements, make several charts instead of overlaying

--- positions/code/visualization.py
+++ positions/code/visualization.py
@@ -6,7 +6,7 @@ import numpy as np
 # Helper methods needed to setup visualization
 
 def predictions_to_json(predictions):
-    axes = ['G', 'G-F', 'F-G', 'F', 'F-C', 'C-F', 'C']
+    axes = ['G', 'G-F', 'F-G', 'F-C', 'C-F', 'C', 'F']
     seasons = [{'className': row[1]['season'], 'axes':[{'axis': axis, 'value': row[1][axis]} for axis in axes]} for row in predictions.iterrows()]
     print seasons
 
