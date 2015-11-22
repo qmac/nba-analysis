32f5f23845f35a6f6f8f92b5d56fd694d6ae3b23
qmac <quinten.mcnamara@gmail.com>
2015-11-22 13:19:49 -0600

    trying new port

--- app.py
+++ app.py
@@ -33,8 +33,5 @@ def get_all_names():
     return json.dumps(names_json)
 
 if __name__ == '__main__':
-    app.run(
-        host="0.0.0.0",
-        port=int("33507"),
-        debug=False
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=int("33507"), debug=False)
