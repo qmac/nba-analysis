11ad2a77c1b8f1daf69a22ba1ce19771fb8f00ce
qmac <quinten.mcnamara@gmail.com>
2016-01-18 02:35:34 -0600

    sanity check

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import positions.code.classification as pos_classification
 import tiers.code.cluster as clstr
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
