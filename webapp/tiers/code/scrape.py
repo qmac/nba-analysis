02fc6e3395d1057ec89456920cee2a0408cd360c
qmac <quinten.mcnamara@gmail.com>
2016-01-28 02:03:17 -0600

    removed picture scraping, minor fixes

--- webapp/tiers/code/scrape.py
+++ webapp/tiers/code/scrape.py
@@ -1,47 +1,12 @@
 import requests
-import csv
-import os
 import sys
-import urllib2
-
-import numpy as np
 import pandas as pd
 
 from webapp import db_engine
 
-def scrape_pictures(players):
-    directory = 'static/pics/'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    for player in players:
-        # Stupid edge cases
-        if player == 'Nene Hilario':
-            player = 'Nene'
-        if player == 'Tim Hardaway':
-            player = 'Tim Hardaway Jr'
-        if player == 'Jose Juan Barea':
-            player = 'Jose Barea'
-
-        url_name = player.lower().replace(' ', '_').replace('.', '').replace('\'', '')
-        print url_name
-
-        url = 'http://i.cdn.turner.com/nba/nba/.element/img/2.0/sect/statscube/players/large/%s.png' % (url_name)
-
-        try:  
-            image = urllib2.urlopen(url)
-            data = image.read()
-
-            with open('%s%s.png' % (directory, url_name), 'w+') as outfile:
-                outfile.write(data)
-
-        except:
-            continue
-
 def scrape_advanced(years):
-    YEARS = ['2014-15', '2015-16']
     dfs = []
-    for year in YEARS:
+    for year in years:
         # URL
         stats_url = 'http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&'\
         'DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Advanced&Month=0&'\
@@ -68,11 +33,9 @@ def scrape_advanced(years):
     print 'Finished updating advanced stats in db'
     return
 
-# scrape_pictures(players)
-
 if __name__ == '__main__':
     if len(sys.argv) != 1:
         print 'Usage: python scrape.py'
         exit(-1)
     
-    scrape_advanced()
+    scrape_advanced(['2014-15', '2015-16'])
