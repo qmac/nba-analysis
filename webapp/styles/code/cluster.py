4c049c6df78340a6f05c4caefc26799cb1aec0d0
qmac <quinten.mcnamara@gmail.com>
2016-08-13 00:29:25 -0600

    getting some cool results, using affinity propagation

--- webapp/styles/code/cluster.py
+++ webapp/styles/code/cluster.py
@@ -1,9 +1,14 @@
 from sklearn.cluster import KMeans
+from sklearn.cluster import AffinityPropagation
 
 import pandas as pd
 
 df = pd.read_csv('playtype_data.csv')
-clstr = KMeans(10)
-clstr.fit(df.drop('Player', 1))
+clstr = AffinityPropagation()
+team_df = df.drop('Player', 1).groupby('Team').mean()
+clstr.fit(df.drop(['Player', 'Team'], 1))
+#clstr.fit(team_df)
+#team_df['cluster'] = clstr.labels_
+#team_df.sort('cluster').to_csv('results.csv')
 df['cluster'] = clstr.labels_
-df[['Player', 'cluster']].sort('cluster').to_csv('results.csv')
+df.to_csv('player_results.csv')
--- webapp/styles/code/scrape.py
+++ webapp/styles/code/scrape.py
@@ -6,18 +6,9 @@ import pandas as pd
 def scrape(url):
     try:
         response_json = requests.get(url, headers={'user-agent': 'Mozilla/5.0', 'referer': 'http://stats.nba.com/scores/'}).json()
+        headers = response_json["resultSets"][0]["headers"]
         data = response_json["resultSets"][0]["rowSet"]
-        return data
-    except:
-        print "error"
-
-    return
-
-def get_headers(url):
-    try:
-        response_json = requests.get(url, headers={'user-agent': 'Mozilla/5.0', 'referer': 'http://stats.nba.com/scores/'}).json()
-        data = response_json["resultSets"][0]["headers"]
-        return data
+        return headers, data
     except:
         print "error"
 
@@ -28,18 +19,23 @@ play_types = ['Transition', 'Isolation', 'PRBallHandler', 'PRRollMan',
             'Postup', 'Spotup', 'Handoff', 'Cut', 'OffScreen', 'OffRebound']
 
 df = pd.DataFrame(columns=play_types)
+player_to_team = {}
 for play_type in play_types:
     url = 'http://stats.nba.com/js/data/playtype/player_%s.js' % (play_type)
-    headers = get_headers(url)
+    headers, data = scrape(url)
+
     player_first_idx = headers.index('PlayerFirstName')
     player_last_idx = headers.index('PlayerLastName')
+    team_idx = headers.index('TeamNameAbbreviation')
     time_idx = headers.index('Time')
 
-    data = scrape(url)
     dicts = {}
     for stats in data:
-        dicts[stats[player_first_idx] + " " + stats[player_last_idx]] = stats[time_idx]
+        full_name = stats[player_first_idx] + " " + stats[player_last_idx]
+        dicts[full_name] = stats[time_idx]
+        player_to_team[full_name] = stats[team_idx]
     df[play_type] = pd.Series(index=dicts.keys(), data=dicts.values())
 
+df['Team'] = pd.Series(index=player_to_team.keys(), data=player_to_team.values())
 df = df.fillna(0)
 df.to_csv('playtype_data.csv', index_label='Player')
