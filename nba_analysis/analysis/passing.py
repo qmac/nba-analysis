48e112b5b355475f83759794eb83993f5fd97e84
qmac <quinten.mcnamara@gmail.com>
2017-02-27 18:22:41 -0600

    fixed trade bug

--- nba_analysis/analysis/passing.py
+++ nba_analysis/analysis/passing.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 from networkx.readwrite import json_graph
 
 import sys
 
 def construct_graph(df):
     graph = nx.DiGraph()
-    graph.add_nodes_from(df['PLAYER_NAME_LAST_FIRST'].unique())
-    passes = df.as_matrix(columns=['PLAYER_NAME_LAST_FIRST', 'PASS_TO', 'FREQUENCY'])
-    twos = df['FG2A']
-    thress = df['FG3A']
-    # Add make and miss shots
-    graph.add_weighted_edges_from(passes, weight='label')
+    players = df['PLAYER_NAME_LAST_FIRST'].unique()
+    graph.add_nodes_from(players)
+    for p in players:
+        player_df = df[df['PLAYER_NAME_LAST_FIRST'] == p]
+        passes = player_df.as_matrix(columns=['PLAYER_NAME_LAST_FIRST', 
+            'PASS_TO', 'FREQUENCY'])
+        # Normalize to account for trades
+        passes[:,[2]] = passes[:,[2]] / passes[:,[2]].sum()
+        graph.add_weighted_edges_from(passes, weight='label')
+    
+    # twos = df['FG2A']
+    # thress = df['FG3A']
     return graph
 
 def get_passing_info(df, team):
@@ -26,7 +33,9 @@ if __name__ == '__main__':
 
     df = pd.DataFrame.from_csv('./../data/passing_data.csv')
     df = df[df['TEAM_NAME'] == sys.argv[1]]
+    construct_graph(df)
     print 'Drawing passing graph using matplotlib'
     nx.draw(construct_graph(df))
+    plt.show()
     print 'Must use web app to run possession simulation'
     
